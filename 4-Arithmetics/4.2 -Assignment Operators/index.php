3e2c08e231f4134491c975a3bf00c43af8f410be
iliasMahraoui <2143218@talnet.nl>
2022-01-12 14:43:06 +0100

    1

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$score = 50;  
+$score += 100;
+
+echo $score;
+?>  
+
+<?php
+$score = 200;  
+$score -= 100;
+
+echo $score;
+?>  
+
+<?php
+$score = 10;  
+$score /= 100;
+
+echo $score;
+?>  
+
+<?php
+$score = 5;  
+$score *= 100;
+
+echo $score;
+?>  
+
+</body>
+</html>
