b00b268e5956503f6fc27e1c35f212e044a2a92b
iliasMahraoui <2143218@talnet.nl>
2022-01-13 12:47:50 +0100

    1

---
+++ 5-Conditional_Statements/5.5 - switch conditional/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$score = 10;
+$score = 9;
+$score = 8;
+$score = 7;
+$score = 6;
+
+
+switch($score) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    case 6:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+?>
+
+</body>
+</html> 
