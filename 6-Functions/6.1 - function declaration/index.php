9f8e2ee196547719d5d0f7a951bc63b00acad0d7
iliasMahraoui <2143218@talnet.nl>
2022-01-13 14:19:53 +0100

    1

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ function zetTweeWoordenAanElkaar($woordje, $zinnetje){
+     return $woordje . " " . $zinnetje;
+}
+$HeleZin = zetTweeWoordenAanElkaar("Hallo","mooie blauwe planeet!");
+echo zetTweeWoordenAanElkaar("Hallo","mooie blauwe planeet!");
+
+?>
+
+</body>
+</html> 
