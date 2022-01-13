19a406bab93e3bfb51a981c58851af1205e75eba
iliasMahraoui <2143218@talnet.nl>
2022-01-13 12:31:17 +0100

    1

---
+++ 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$prod_besteld = 10;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+echo "1.50 $prijs 1.00"
+?>
+
+</body>
+</html> 
