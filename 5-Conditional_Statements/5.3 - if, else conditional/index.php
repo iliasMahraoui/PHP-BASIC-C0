87a25a29217eefd5f8f687f1a7948e571b89ddf8
iliasMahraoui <2143218@talnet.nl>
2022-01-13 12:08:24 +0100

    1

---
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 == $test2) {
+    echo "gelijk";
+}
+else {
+    echo "niet gelijk";
+}
+?>
+
+<br>
+
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 != $test2) {
+    echo "gelijk";
+}
+else {
+    echo "niet gelijk";
+}
+?>
+
+<br>
+
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 === $test2) {
+    echo "gelijk";
+}
+else {
+    echo "niet gelijk";
+}
+?>
+
+<br>
+
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 > $test2) {
+    echo "gelijk";
+}
+else {
+    echo "niet gelijk";
+}
+?>
+
+<br>
+
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 < $test2) {
+    echo "gelijk";
+}
+else {
+    echo "niet gelijk";
+}
+?>
+
+<br>
+
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 >= $test2) {
+    echo "gelijk";
+}
+else {
+    echo "niet gelijk";
+}
+?>
+
+<br>
+
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 <= $test2) {
+    echo "gelijk";
+}
+else {
+    echo "niet gelijk";
+}
+?>
+
+</body>
+</html> 
