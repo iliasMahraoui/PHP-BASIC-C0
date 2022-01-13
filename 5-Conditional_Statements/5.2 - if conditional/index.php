436c29588314e8f74eafc20bf5976ff96b02a907
iliasMahraoui <2143218@talnet.nl>
2022-01-13 11:36:05 +0100

    1

---
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$score1 = 10;
+$score2 = 10;
+
+if($score1 == $score2) {
+    echo "ongelijk";
+}
+?>
+
+<br>
+
+<?php
+$score1 = 10;
+$score2 = 10;
+
+if($score1 == $score2) {
+    echo "identiek";
+}
+?>
+
+<br>
+
+<?php
+$score1 = 10;
+$score2 = 10;
+
+if($score1 == $score2) {
+    echo "test1 is groter dan test2";
+}
+?>
+
+<br>
+
+<?php
+$score1 = 10;
+$score2 = 10;
+
+if($score1 == $score2) {
+    echo "test1 is groter of gelijk aan test2";
+}
+?>
+
+<br>
+
+</body>
+</html> 
